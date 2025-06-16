628e6431c228d6b87889c17f1ae1eb4779b8c800
Krishnan <krishnan@adv8transformations.com>
2025-06-16 15:34:35 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(`\MongoDb connected : ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
+
+// import express from "express";
+// const app = express()
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Err :", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw err;
+//   }
+// })();
